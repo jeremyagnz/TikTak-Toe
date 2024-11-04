2c63deb598e3782f0f45ebcaf1e88a1de2708c09
Jeremy Arias <jeremyagnz@gmail.com>
2024-11-03 21:48:17 -0400

    Applying Winner and Reset Game

--- tiktak-toe/src/App.jsx
+++ tiktak-toe/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 
-const TURNS ={
+const TURNS = {
   x: '×',
-  O:'o'
+  O: 'o'
 };
 
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
-  
+
   const className = `square ${isSelected ? 'is-selected' : ''}`
   const handledClick = () => {
     updateBoard(index);
@@ -20,25 +20,65 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+export const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.x);
 
+  const [winner, setwinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+
+      if (boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.x);
+    setwinner(null);
+  }
+
   const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-    
-    const newTurn = turn === TURNS.x? TURNS.O : TURNS.x;
-    setTurn(newTurn)
+
+    const newTurn = turn === TURNS.x ? TURNS.O : TURNS.x;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setwinner(newWinner)
+    }
   }
 
-  
+
   return (
     <main className='board'>
-       <h1>Tik Tak Toe</h1>
-       <section className="game">
+      <h1>Tik Tak Toe</h1>
+     <button onClick={resetGame}>Reiniciar el Juego</button>
+      <section className="game">
         {
           board.map((_, index) => {
             return (
@@ -53,14 +93,36 @@ function App() {
           })
         }
       </section>
-      <div className="turn">
+      <section className="turn">
         <Square isSelected={turn === TURNS.x}>
           {TURNS.x}
         </Square>
-         <Square isSelected={turn === TURNS.O}>
+        <Square isSelected={turn === TURNS.O}>
           {TURNS.O}
         </Square>
-      </div>
+      </section>
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó:'
+                }
+              </h2>
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
